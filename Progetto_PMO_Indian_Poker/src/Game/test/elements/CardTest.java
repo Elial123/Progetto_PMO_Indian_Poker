a82cc6e2d2fd16cb083f558e6e5565aa0e257c3a
elial <elial@LAPTOP-ID8MANQ8>
2024-09-11 15:22:25 +0200

    update

--- Progetto_PMO_Indian_Poker/src/Game/test/elements/CardTest.java
+++ Progetto_PMO_Indian_Poker/src/Game/test/elements/CardTest.java
@@ -8,28 +8,37 @@ import Game.project.resources.model.elements.Card;
 
 public class CardTest {
 	private Card card;
+	private int seed,
+				value;
 	
 	@BeforeEach                                               
     void setUp() {
-		card = new Card((int)(Math.random() * (9 - 0)) + 0, (int)(Math.random() * (3 - 0)) + 0);
+		value = (int)(Math.random() * (9 - 0)) + 0;
+		seed = (int)(Math.random() * (3 - 0)) + 0;
+		card = new Card(value, seed);
     }
 	
 	
 	// test costruttore e metodo getValue()
 	@Test
 	void testGetValue() {
-		Assert.assertNotNull(card.getValue());
+		Assert.assertEquals(value, card.getValue());
 	}
 	
 	// test metodo get
 	@Test
 	void testGetSeed() {
-		Assert.assertNotNull(card.getSeed());
+		Assert.assertEquals(seed, card.getSeed());
 	}
 	
 	@Test
 	void testToString() {
-		Assert.assertNotNull(card.toString());
+		String[] values ={"1", "2", "3", "4", "5", "6", "7", "8", "9", "10"};
+		String[] seeds ={"quadri","cuori","fiori","picche"};
+		
+		String str = values[value] + " di " + seeds[seed];
+		
+		Assert.assertEquals(str, card.toString());
 	}
 
 }
